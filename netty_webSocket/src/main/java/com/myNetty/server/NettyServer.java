b11631cda2e4cfcd9a4e1b7695d85017e4a03455
songjialun <silver9360@163.com>
2018-10-12 11:05:38 +0800

    开始剥离 spring和tomcat  项目分包

---
+++ netty_webSocket/src/main/java/com/myNetty/server/NettyServer.java
@@ -0,0 +1,97 @@
+package com.myNetty.server;
+
+import com.myNetty.manage.ServerChannelsHolder;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.util.concurrent.ImmediateEventExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * netty_webSocket服务的启动类
+ */
+public class NettyServer {
+
+    private static final Logger logger = LoggerFactory.getLogger(NettyServer.class);
+    // serverChannel的执行线程组，线程数量为1
+    private static final NioEventLoopGroup BOSS;
+    // clientChannel的执行线程组，线程数量使用默认值
+    private static final NioEventLoopGroup WORKER;
+    // 监听端口号
+    private int port;
+    
+    public NettyServer(Integer port) {
+        this.port = port;
+    }
+    
+    static {
+        BOSS =  new NioEventLoopGroup(1);
+        WORKER = new NioEventLoopGroup();
+    }
+
+    public void start() throws Throwable {
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(BOSS, WORKER)
+                .channel(NioServerSocketChannel.class)
+                /*
+                 * ChannelOption.SO_BACKLOG对应的是TCP/IP协议listen函数中的backlog参数，
+                 * 函数listen(int socketfd,int backlog)用来初始化服务端可连接队列，服务端处理客户端连接请求是顺序处理的，所以同一时间只能处理一个客户端连接，
+                 * 多个客户端来的时候，服务端将不能处理的客户端连接请求放在队列中等待处理，backlog参数指定了队列的大小
+                 */
+                .option(ChannelOption.SO_BACKLOG, 128)
+                // 使用默认的 对象池
+                .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                .handler(new LoggingHandler(LogLevel.DEBUG))
+                // 禁用Nagle算法 降低TCP消息延迟
+                .childOption(ChannelOption.TCP_NODELAY, true)
+                // 连接保持 KEEP-ALIVE 状态
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                // 使用默认的 对象池
+                .childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                // 自定义handler 处理连接
+                .childHandler(new WebSocketChannelInitializer());
+
+            ChannelFuture future = serverBootstrap.bind(port).syncUninterruptibly();
+            logger.info("The netty webSocket com.myNetty.server is now ready to accept requests on port {}", NettyServer.this.port);
+            // 初始化群组
+            ServerChannelsHolder.getChannelGroups().put("all", new DefaultChannelGroup(ImmediateEventExecutor.INSTANCE));
+            // 阻塞关闭
+            future.channel().closeFuture().syncUninterruptibly();
+        } catch (Exception e) {
+            logger.error("The netty webSocket com.myNetty.server start error on {}", e.getMessage());
+            throw new Throwable(e);
+        } finally {
+            // 释放资源
+            destroy();
+        }
+    }
+
+    /**
+     * 当容器关闭时 强制结束所有netty server相关的进程
+     */
+    private void destroy() {
+        try {
+            if (!BOSS.isShutdown()) {
+                BOSS.shutdownGracefully();
+            }
+            if (!WORKER.isShutdown()) {
+                WORKER.shutdownGracefully();
+            }
+        } catch (Exception e) {
+            logger.info("Fail to kill thread of the netty com.myNetty.server");
+            logger.error(e.getMessage());
+        }
+    }
+}
